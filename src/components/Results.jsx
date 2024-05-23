9a86af6faeb20be43f2f739711115f2597c6bb84
karthik <jkarthik1947@gmail.com>
2024-05-23 14:27:49 +0530

    Fetch data from tmdb api

---
+++ src/components/Results.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Results( { results }) {
+  return (
+    <div>
+        {
+            results.map((result) => (
+                <div key={result.id}>
+                    <h2>{result.original_title}</h2>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
