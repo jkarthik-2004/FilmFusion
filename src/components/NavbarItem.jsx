da792b515862e843abc414271883841f4af921c8
karthik <jkarthik1947@gmail.com>
2024-05-23 17:01:13 +0530

    useSearchParams wrapped with suspense in the NavbarItem.jsx

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -3,16 +3,23 @@ import Link from 'next/link'
 import React, { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-export default function NavbarItem( { title, param }) {
- 
-    const SearchParams = useSearchParams()
-    const genre = SearchParams.get('genre')
+function NavbarItemContent({ title, param }) {
+  const searchParams = useSearchParams()
+  const genre = searchParams.get('genre')
+  
   return (
     <div>
-      <Suspense>
-        <Link
-        className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg': ''}`} 
-        href={`/?genre=${param}`}>{title}</Link></Suspense>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''}`} 
+        href={`/?genre=${param}`}>{title}</Link>
     </div>
+  )
+}
+
+export default function NavbarItem({ title, param }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavbarItemContent title={title} param={param} />
+    </Suspense>
   )
 }
