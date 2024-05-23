4a9697362c2db9de4199eb13e78245e08bf0a485
karthik <jkarthik1947@gmail.com>
2024-05-23 13:08:20 +0530

    Added Dark Mode

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,18 @@
+'use client'
+import { MdLightMode, MdDarkMode } from 'react-icons/md'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+export default function DarkModeSwitch() {
+    const { theme, setTheme, systemTheme} = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    const currentTheme = theme === 'system' ? systemTheme : theme
+    useEffect(()=>setMounted(true), [])
+  return (
+    <div>
+        {
+            mounted && (currentTheme === 'dark'? <MdLightMode className='text-xl cursor-pointer hover:text-amber-500' onClick={()=>setTheme('light')}/> : <MdDarkMode className='text-xl cursor-pointer hover:text-amber-500' onClick={()=>setTheme('dark')}/>)
+        }
+    </div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
 
 export default function Header() {
   return (
@@ -11,10 +12,13 @@ export default function Header() {
             <MenuItem title="home" address="/" Icon={AiFillHome}/>
             <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill}/>
         </div>
+        <div className="flex items-center gap-4">
+            <DarkModeSwitch/>
         <Link href={"/"} className="flex gap-1 items-center">
              <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>Film</span>
              <span className='text-xl hidden sm:inline'>Fusion</span>
         </Link>
+        </div>
     </div>
   )
 }
