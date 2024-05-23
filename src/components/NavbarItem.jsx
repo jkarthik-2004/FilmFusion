24bc20d49585a9759991bbcbb431ee1c5a9fc0f9
karthik <jkarthik1947@gmail.com>
2024-05-23 16:50:39 +0530

    Added suspense boundary to useSearchParams

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,16 +1,18 @@
 'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 
 export default function NavbarItem( { title, param }) {
+ 
     const SearchParams = useSearchParams()
     const genre = SearchParams.get('genre')
   return (
     <div>
+      <Suspense>
         <Link
         className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg': ''}`} 
-        href={`/?genre=${param}`}>{title}</Link>
+        href={`/?genre=${param}`}>{title}</Link></Suspense>
     </div>
   )
 }
