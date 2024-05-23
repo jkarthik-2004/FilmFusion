000c25aa2d70d85dd94bafea1e51f90dde3d55ed
karthik <jkarthik1947@gmail.com>
2024-05-23 17:12:09 +0530

    Added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,13 +8,16 @@ import SearchBox from "@/components/SearchBox";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Movie App",
-  description: "This is a Movie Suggestion Website",
+  title: "Film Fusion",
+  description: "This is a Movie info website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <link rel="icon" href="mv-icon.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <Providers>
           <Header/>
